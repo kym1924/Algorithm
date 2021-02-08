8da8efc9c1f84256870de2e9ba3d2019cec6d657
kym1924 <kym1924@naver.com>
2021-02-09 01:30:02 +0900

    Add 3273

---
+++ BOJ/BOJ_3273.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_3273 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int answer = Integer.parseInt(br.readLine());
+
+		Arrays.sort(arr);
+
+		int count = 0;
+		int left = 0, right = arr.length - 1;
+
+		while (left < right) {
+			int check = arr[left] + arr[right];
+			if (check == answer) {
+				count++;
+				right--;
+			} else if (check > answer) {
+				right--;
+			} else {
+				left++;
+			}
+		}
+
+		System.out.print(count);
+	}
+}
