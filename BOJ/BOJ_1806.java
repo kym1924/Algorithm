cbb48a4ea9b46fd52806f289e9aea0f9a5ff1634
kym1924 <kym1924@naver.com>
2021-02-10 20:21:42 +0900

    Add 1806

---
+++ BOJ/BOJ_1806.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1806 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0, right = 0, check = 0;
+		int min = Integer.MAX_VALUE;
+
+		while (right <= N) {
+			if (check >= S) {
+				min = Math.min(min, right - left + 1);
+				check -= arr[left++];
+			} else if (check < S) {
+				check += arr[right++];
+			}
+		}
+
+		if (min == Integer.MAX_VALUE) {
+			System.out.print(0);
+		} else {
+			System.out.print(min);
+		}
+	}
+
+}
