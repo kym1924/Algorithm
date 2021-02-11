5c26007e9a95525f9195e68cb627789fb22f851e
kym1924 <kym1924@naver.com>
2021-02-11 23:06:08 +0900

    Add 2798

---
+++ BOJ/BOJ_2798.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_2798 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] card = new int[N];
+		int result = 0;
+		int max = 0;
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					if (card[i] != card[j] && card[i] != card[k] && card[j] != card[k]) {
+						result = card[i] + card[j] + card[k];
+					}
+					if (result <= M && max < result) {
+						max = result;
+					}
+				}
+			}
+		}
+
+		System.out.print(max);
+	}
+}
