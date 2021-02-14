266d21f57e0431f1eb8abf3ea02b2c58911abaf6
kym1924 <kym1924@naver.com>
2021-02-14 22:31:01 +0900

    Add 1747

---
+++ BOJ/BOJ_1747.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1747 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		if (N == 1) {
+			System.out.print(2);
+			return;
+		}
+
+		for (int i = N; i <= 1500000; i++) {
+			if (isPalindrome(i)) {
+				if (isPrime(i)) {
+					System.out.print(i);
+					break;
+				}
+			}
+		}
+	}
+
+	public static boolean isPrime(int N) {
+		for (int i = 2; i < N / 2; i++) {
+			if (N % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isPalindrome(int N) {
+		StringBuilder str = new StringBuilder();
+		str.append(N);
+		if (str.toString().equals(str.reverse().toString())) {
+			return true;
+		}
+		return false;
+	}
+}
