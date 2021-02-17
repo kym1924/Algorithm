ce230cc26a066f1ed46773fcfd5268f7b1018cf2
kym1924 <kym1924@naver.com>
2021-02-18 00:25:40 +0900

    Add 7568

---
+++ BOJ/BOJ_7568.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_7568 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int[][] people = new int[N][N];
+		int[] result = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			people[i][0] = Integer.parseInt(st.nextToken());
+			people[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i < N; i++) {
+			int grade = 1;
+			for (int j = 0; j < N; j++) {
+				if (i != j) {
+					if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+						grade++;
+					}
+				}
+			}
+			result[i] = grade;
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + " ");
+		}
+	}
+}
