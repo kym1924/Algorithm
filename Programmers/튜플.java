34b4572c441fa3fee2190197b7d0cdeb4e6df905
kym1924 <kym1924@naver.com>
2021-04-20 22:52:39 +0900

    Add 튜플

---
+++ Programmers/튜플.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+class Solution {
+	public int[] solution(String s) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		String[] temp = s.substring(1, s.length() - 2).split("},");
+
+		for (int i = 0; i < temp.length; i++) {
+			String[] now = temp[i].replace("{", "").split(",");
+			for (int j = 0; j < now.length; j++) {
+				int index = Integer.parseInt(now[j]);
+				if (map.get(index) != null) {
+					int count = map.get(index);
+					map.put(index, count + 1);
+				} else {
+					map.put(index, 1);
+				}
+			}
+		}
+
+		List<Integer> list = new ArrayList<>(map.keySet());
+		Collections.sort(list, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
+
+		return list.stream().mapToInt(Integer::intValue).toArray();
+	}
+}
